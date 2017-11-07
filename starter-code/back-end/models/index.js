7c3e517e7b1b45d280f34ca3a6ea75e88418ff94
Courtney Fay <fay.courtney@gmail.com>
2017-11-07 16:05:40 -0700

    adds sprint 1 work

--- starter-code/back-end/models/index.js
+++ starter-code/back-end/models/index.js
@@ -1,7 +1,7 @@
 //Connect
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://<username>@localhost:5432/tunr_relationships');
+var sequelize = new Sequelize('postgres://cfay@localhost:5432/tunr_relationships');
 
 //Export models and Sequelize for seed and dbSetup
 module.exports.Sequelize = Sequelize;
@@ -10,6 +10,9 @@ module.exports.sequelize = sequelize;
 var Artist = sequelize.import("./artist");
 var Manager = sequelize.import("./manager");
 var Song = sequelize.import("./song");
+
+Artist.hasMany(Song);
+Song.belongsTo(Artist);
 
 module.exports.models = {
 	Artist: Artist,
